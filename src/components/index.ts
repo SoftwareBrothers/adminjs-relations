e354a2bcae09065e5d270b242ce2527d58cefd05
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-02-24 15:35:00 +0100

    chore: add draft relation feature

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as RecordRelationsComponent } from './RecordRelations';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /**
- * @module @adminjs/example-feature
+ * @module @adminjs/relations
  * @subcategory Features
  * @section modules
  */
 
-import feature from './feature';
+import relationsFeature from './relations.feature';
 
-export default feature;
+export default relationsFeature;
